2ec9d16b5377df92b4b5ac863171e31ae4030a7f
creatorsummaries <mattrobm@gmail.com>
2025-03-13 19:28:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello from Creator Summaries!");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Creator Summaries via Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
