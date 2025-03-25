f6d9bdd74409c22ae052bb24f9d0fa06b4672501
creatorsummaries <mattrobm@gmail.com>
2025-03-24 19:18:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Creator Summaries via Express!');
+  res.send("Hello from Creator Summaries via Express!");
 });
 
 app.listen(PORT, () => {
